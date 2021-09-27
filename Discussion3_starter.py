b47cbd8faf2adaa1d8f3af3f510ed5785b8a1a96
Jacobnegroni <89854940+Jacobnegroni@users.noreply.github.com>
2021-09-26 22:19:22 -0400

    Add files via upload

--- Discussion3_starter.py
+++ Discussion3_starter.py
@@ -3,6 +3,10 @@ import random
 
 # create the class Dice
 class Dice:
+    def __init__(self, sides=6, tracker):
+        self.num_sides = sides
+        self.tracker = []
+    
     # create the constructor (__init__) method
     # it takes as input the number sides and if none is specified use 6
     # it sets the dice object's number of sides (instance variable)
@@ -11,9 +15,15 @@ class Dice:
 
     # create the __str__ method
     # it returns "Last roll: value" where value is the last value in the list that tracks the rolls
+    def __str__(self):
+        return "Last roll: " + str(self.tracker[-1])
 
 
     # create the roll method
+    def roll(self):
+        num = random.randint(1, self.num_sides)
+        self.tracker.append(num)
+        return num
     # it randomly picks a value from 1 to the number of sides this dice object has
     # it adds that value to the end of the list that tracks all the rolls
     # it returns the value
